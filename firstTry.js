94910fb901cae61f69525f40b4901b2f94fc22f7
范佳伟 <fjwdyx123@163.com>
2017-05-17 12:42:51 +0800

    Revert "Day-2"
    This reverts commit 8da9c5deba9504918547b95593d5de84efe620db.

--- firstTry.js
+++ firstTry.js
@@ -16,10 +16,8 @@
 
     //对象的构造函数
     function Init(para){
-        this.init(para);
         //设置私有属性jsObj,储存对应的js对象
-        //this.jsObj = this.init(para);
-        //this.length = this.jsObj.length;
+        this.jsObj = this.init(para);
     };
 
 
@@ -80,42 +78,18 @@
                     arr.push(para);
                 }
             }
-            for(var i = 0;i<arr.length;i++)
-            {
-                this[i] = arr[i];     //this指jq对象（构造函数Init），对jq对象下标拓展指向js对象
-            }
-            this.length = arr.length;
-        },
-        //size   返回jq对象长度
-        size : function(){
-            return this.length;
-        },
-        //get    jq对象的第i个js对象返回js对象
-        get : function(i){
-            return  this[i];
+            return arr;
         },
 
-        //each   遍历
-        each : function(x){
-            for(var i = 0;i<this.length;i++){
-                x.call( this[i],i );
-            }
-        },
-        //eq   jq对象的第i个js对象返回jq对象
-        eq : function(i){
-            console.log((this[i]).length);
-            return  $(this[i]);
-        },
-
-        //CSS   css样式修改/获取
+        //CSS
         css : function(){
             var prop = arguments;
             if(prop.length == 2)
             {
-                this.each(function(){
-                    this.style[prop[0]] = prop[1];
-                });
-                return this;
+                for(var i = 0;i<this.jsObj.length;i++)
+                {
+                    this.jsObj[i].style[prop[0]] = prop[1];
+                }
             }
             else if(prop.length == 1)
             {
@@ -123,107 +97,23 @@
                 if(x == 'object')
                 {
                     for(key in prop[0]) {
-                        this.each(function(){
-                            this.style[key] = prop[0][key];
-                        });
+                        for (var i = 0; i < this.jsObj.length; i++) {
+                            this.jsObj[i].style[key] = prop[0][key];
+                        }
                     }
-                    return this;
                 } else if(  x == 'string')
                 {
-                    return this[0].currentStyle?this[0].currentStyle[prop[0]]:getComputedStyle(this[0])[prop[0]];
-                }
-            }
-        },
-        //html
-        html : function(x){
-            if(x != undefined)              //修改jq对象的html内容
-            {
-                this.each(function(){
-                    this.innerHTML = x;
-                });
-                return this;
-            }
-            else{                           //获取jq独享第一个js对象的html内容
-                return this[0].innerHTML;
-            }
-        },
-        //Text
-        text : function(x){
-            if(x != undefined)
-            {
-                this.each(function(){
-                    this.innerText = x;
-                });
-                return this;
-            }
-            else{
-                var str ='';
-                this.each(function(){
-                    str += this.innerText;
-                });
-                return str;
-            }
-        },
-
-        //attr     属性修改/获取
-        attr : function(){
-            var prop = arguments;
-            if(prop.length == 2)
-            {
-
-                this.each(function(){
-                    this.setAttribute( prop[0],prop[1] );
-                });
-                return this;
-            }
-            else if(prop.length == 1)
-            {
-                var x = (typeof prop[0]).toLowerCase();
-                if(x == 'object')
-                {
-                    for(key in prop[0]) {
-                        this.each(function(){
-                            this.setAttribute( key ,prop[0][key] );
-                        });
-                    }
-                    return this;
-                } else if(  x == 'string')
-                {
-                    return this[0].getAttribute(prop[0]);
-                }
-            }
-        },
-        //prop     HTML默认属性修改/获取
-        prop : function(){
-            var x = arguments;
-            if(x.length == 2)
-            {
-                if(x[0].toLowerCase() == 'class')x[0]='className';
-                this.each(function(){
-                    this[x[0]] = x[1];
-                });
-                return this;
-            }
-            else if(x.length == 1)
-            {
-                var y = (typeof x[0]).toLowerCase();
-                if(y == 'object')
-                {
-                    for(key in x[0]) {
-                        if(key.toLowerCase() == 'class')key = 'className';
-                        this.each(function(){
-                            this[key] = x[0][key];
-                        });
-                    }
-                    return this;
-                } else if(  y == 'string')
-                {
-                    return this[0][x[0]];
+                    return this.jsObj[0].currentStyle?this.jsObj[0].currentStyle[prop[0]]:getComputedStyle(this.jsObj[0])[prop[0]];
                 }
             }
         },
 
-    };
+        //eq
+        eq : function(i){
+            console.log((this.jsObj[i]).length);
+            return  $(this.jsObj[i]);
+        }
+    },
 
     window.$ = $;
 }();
